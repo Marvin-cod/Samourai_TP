7fb4027af9e16581bef7a26704cf86ffd8017cf4
marvi <marvi@172.17.138.201>
2022-11-10 14:42:03 +0100

    save

---
+++ src/personnages/Humain.java
@@ -0,0 +1,46 @@
+package personnages;
+
+public class Humain {
+	
+	private String nom;
+	private String boisson;
+	private int argent;
+	
+	public Humain (String nom, String boisson, int argent) {
+		this.nom=nom;
+		this.boisson=boisson;
+		this.argent=argent;
+	}
+	
+		
+	
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public String getBoisson() {
+		return boisson;
+	}
+
+	
+	public int getArgent() {
+		return argent;
+	}
+
+
+	public void Parler(String texte) {
+		System.out.println("<"+this.nom+">"+texte);
+	}
+	
+	public void Bonjour() {
+		Parler("Bonjour je m'appelle "+this.nom+" et j'aime boire du "+this.boisson);
+	}
+	
+	public void Boire() {
+		Parler("MMMMmmmm un bon verre de "+this.boisson+" ! Gloups");
+	}
+	
+	
+}
